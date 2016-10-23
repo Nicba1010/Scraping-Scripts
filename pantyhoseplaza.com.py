2ed024818a4a9916ca38a4a830d4f2c4537d9068
Root @ France Server <nicba1010@gmail.com>
2016-10-24 01:06:12 +0200

    Separated functions out of the first script. Added the wotd script as per /u/nsq1 's request. Note: Works only for dates from 2014/03/01

--- pantyhoseplaza.com.py
+++ pantyhoseplaza.com.py
@@ -1,52 +1,9 @@
 #!/usr/bin/env python
 import urllib2, base64, sys, re, os, json
 from bs4 import BeautifulSoup
+from functions import outputcolors, ensureDir, fileDlWithAuth
 
-class outputcolors:
-	OKGREEN = '\033[92m'
-	OKBLUE = '\033[94m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-
-def ensureDir(f):
-	if not os.path.exists(f):
-		os.makedirs(f)
-def fileDlWithAuth(url, auth, dir, prepend):
-	fileName = url.split('/')[-1]
-	request = urllib2.Request(url)
-	request.add_header("Authorization", "Basic %s" % auth)
-	u = urllib2.urlopen(request)
-	meta = u.info()
-	fileSize = -1
-	try:
-		fileSize = int(meta.getheaders("Content-Length")[0])
-	except Exception:
-		pass
-	if os.path.exists(dir + fileName):
-		if os.stat(dir + fileName).st_size == fileSize:
-			print(prepend + outputcolors.OKBLUE + "File already downloaded!" + outputcolors.ENDC)
-			return
-	else:
-		print(prepend + outputcolors.WARNING + "File downloaded but not fully! Restarting download..." + outputcolors.ENDC)
-	fileHandle = open(dir + fileName, 'wb')
-	print (prepend + ("Downloading: %s Bytes: %s" % (fileName, "???" if (fileSize == -1) else fileSize)))
-	fileSizeDl = 0
-	blockSize = 65536
-	while True:
-		buffer = u.read(blockSize)
-		if not buffer:
-			break
-		fileSizeDl += len(buffer)
-		fileHandle.write(buffer)
-		status = r"%10d  [%3.2f%%]" % (fileSizeDl, -1.0 if (fileSize == -1) else (fileSizeDl * 100. / fileSize))
-		status = prepend + status
-		status = status + chr(8)*(len(status) + 1)
-		print status,
-	fileHandle.close()
-	print(prepend + outputcolors.OKGREEN + "Done :)" + outputcolors.ENDC)
-
-if len(sys.argv) < 3:
+if len(sys.argv) != 4:
 	print("Scraping script for pantyhoseplaza.com porn size.")
 	print("Usage:\n\tpython pantyhoseplaza.com.py pageNumber username password")
 	sys.exit()
---
+++ wotd.dictionary.com.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import urllib2, sys
+from bs4 import BeautifulSoup
+from functions import getSoup, daterange, fileDl, ensureDir
+from datetime import date, datetime
+
+def dlForDate(singleDate):
+	print("Getting Word of the Day for: " + singleDate.strftime("%Y/%m/%d"))
+        wordSoup = getSoup("http://www.dictionary.com/wordoftheday/" + singleDate.strftime("%Y/%m/%d") + "/")
+        url = wordSoup.find('meta', { "property" : "og:image" })['content']
+        print("\tDownloading:" + url)
+        fileDl(url, sys.argv[1], "\t\t")
+
+if (len(sys.argv) < 2) or sys.argv[1] == "--help":
+	print("Scraping script for dictionary.com/wordoftheday/ first WOTD: 1999/5/3")
+	print("Usage:\n\tpython wotd.dictionary.com.py \"/mnt/what/ever/directory/\"")
+	print("\tSpecific date: python wotd.dictionary.com.py \"/mnt/what/ever/directory/\" yyyy/mm/dd -single")
+	print("\tDate range: python wotd.dictionary.com.py \"/mnt/what/ever/directory/\" yyyy/mm/dd yyyy/mm/dd")
+
+startDate = date(1999, 5, 3)
+endDate = date.today()
+ensureDir(sys.argv[1])
+
+if len(sys.argv) == 4 and sys.argv[3] == "-single":
+	startDate = datetime.strptime(sys.argv[2], "%Y/%m/%d")
+	print startDate
+	dlForDate(startDate)
+	sys.exit()
+elif len(sys.argv) == 4 and sys.argv[3] != "-single":
+	startDate = datetime.strptime(sys.argv[2], "%Y/%m/%d")
+	endDate = datetime.strptime(sys.argv[3], "%Y/%m/%d")
+
+for singleDate in daterange(startDate, endDate):
+	dlForDate(singleDate)
